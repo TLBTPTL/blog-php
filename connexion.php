a0da1ec3bb049b0f8b0a0f61c7edefb9351cb159
Jérémy Girard <jeremy.girard@etu.univ-lyon1.fr>
2023-09-27 09:51:31 +0200

    Push de fou
    Ajout de différents trucs

--- connexion.php
+++ connexion.php
@@ -2,10 +2,24 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <link rel="stylesheet" href="css/douze.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Connexion - Blog</title>
 </head>
 <body>
+
+    <form method="post" action="index.html">
+    <div class="box">
+    <h1>Formulaire Connexion</h1>
+
+    <input type="email" name="email" value="email" onFocus="field_focus(this, 'email');" onblur="field_blur(this, 'email');" class="email" />
+    <input type="password" name="email" value="email" onFocus="field_focus(this, 'email');" onblur="field_blur(this, 'email');" class="email" />
     
+    <a href="#"><div class="btn">Se connecter</div></a>
+    
+    </div>  
+    </form>
+
+    <p>Forgot your password? <u style="color:#f1c40f;">Click Here!</u></p>
 </body>
 </html>
--- index.php
+++ index.php
@@ -1,11 +1,19 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
+    <link rel="stylesheet" href="css/style.css">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Liste Article - Blog</title>
 </head>
-<body>  
+<body> 
+    <?php
+        include_once('header.php');
+    ?>
     
+
+    <?php
+        include_once("footer.php");
+    ?>
 </body>
 </html>
