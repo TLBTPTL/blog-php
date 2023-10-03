016e74b40ae31265f0e030098c3d834af0cbd4f3
p2203596 <eliott.sauvaget@etu.univ-lyon1.fr>
2023-10-03 17:57:40 +0200

    Ajout récupération objet article

--- pageArticle.php
+++ pageArticle.php
@@ -1,12 +1,21 @@
+<?php
+    include 'header.php';
+    include 'Aricle.php';
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Article - Blog</title>
+    <title>Page Article - Blog</title>
 </head>
 <body>
-    <div id="Article"> <?php $_Article ?> </div>
+    <div id="Article"> 
+        <?php 
+            $Article = $_SESSION[''];
+        ?> 
+    </div>
     <div id="Commentaire"> <?php $_Commentaire?> </div>
 </body>
 </html>
