a1b96fbc4dbf70453bc2abaa249d710ac4edd4d6
Jérémy Girard <jeremy.girard@etu.univ-lyon1.fr>
2023-10-10 23:58:27 +0200

    Deuxième commit du filtre
    Dis moi que tu le vois Titouan

--- index.php
+++ index.php
@@ -4,18 +4,18 @@ include_once 'functionDataBase.php';
 $connexion = connectToDatabase();
 $_SESSION['erreurconnexion'] = 0;
 
-$selectedCategory = null;
+$categorieSelectionnee = null;
 
 if (isset($_GET['categorie'])) {
-    $selectedCategory = $_GET['categorie'];
-    if ($selectedCategory === "") {
+    $categorieSelectionnee = $_GET['categorie'];
+    if ($categorieSelectionnee === "") {
         $sql = "SELECT idArticle, titreArticle, descriptionArticle, categorieArticle, pseudo FROM article";
     } else {
         $sql = "SELECT idArticle, titreArticle, descriptionArticle, categorieArticle, pseudo FROM article WHERE categorieArticle = :categorie";
     }
     $stmt = $connexion->prepare($sql);
-    if ($selectedCategory !== "") {
-        $stmt->bindParam(':categorie', $selectedCategory);
+    if ($categorieSelectionnee !== "") {
+        $stmt->bindParam(':categorie', $categorieSelectionnee);
     }
     $stmt->execute();
     $articles = $stmt->fetchAll(PDO::FETCH_ASSOC);
@@ -31,11 +31,11 @@ $categories = $stmt->fetchAll(PDO::FETCH_COLUMN);
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Page Article - Blog</title>
+    <title>Page d'Articles - Blog</title>
     <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
@@ -43,9 +43,9 @@ $categories = $stmt->fetchAll(PDO::FETCH_COLUMN);
         <label for="categorie">Filtrer par catégorie :</label>
         <select name="categorie" id="categorie">
             <option value="">Toutes les catégories</option>
-            <?php foreach ($categories as $category) : ?>
-                <option value="<?= $category; ?>" <?= $selectedCategory === $category ? 'selected' : ''; ?>>
-                    <?= $category; ?>
+            <?php foreach ($categories as $categorie) : ?>
+                <option value="<?= $categorie; ?>" <?= $categorieSelectionnee === $categorie ? 'selected' : ''; ?>>
+                    <?= $categorie; ?>
                 </option>
             <?php endforeach; ?>
         </select>
@@ -55,8 +55,8 @@ $categories = $stmt->fetchAll(PDO::FETCH_COLUMN);
     <?php foreach ($articles as $article) : ?>
         <a href='article.php?id=<?= $article['idArticle'] ?>' class='article'>
             <h2><?= $article['titreArticle'] ?></h2>
-            <p><strong>Catégorie:</strong> <?= $article['categorieArticle'] ?></p>
-            <p><strong>Auteur:</strong> <?= $article['pseudo'] ?></p>
+            <p><strong>Catégorie :</strong> <?= $article['categorieArticle'] ?></p>
+            <p><strong>Auteur :</strong> <?= $article['pseudo'] ?></p>
             <p><?= $article['descriptionArticle'] ?></p>
         </a>
     <?php endforeach; ?>
