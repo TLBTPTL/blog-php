0ab069ec7cf21a8aa04963e704437c620b97f9e0
Jérémy Girard <jeremy.girard@etu.univ-lyon1.fr>
2023-10-10 16:34:25 +0200

    Changement traitementarticle.php
    a

--- traitementarticle.php
+++ traitementarticle.php
@@ -1,12 +1,20 @@
 
     <?php
+    session_start();
     if ($_SERVER["REQUEST_METHOD"] == "POST"){
         $_SESSION['titre']=$_POST['titre'];
         $_SESSION[$i.'categorie']=$_POST[$i.'categorie'];
         $_SESSION['contenu']=$_POST['text'];
 
-
-        $sql = mysql_query("INSERT INTO article (idArticle, titreArticle, descriptionArticle, categorieArticle, pseudo, commentaires) VALUES (null, '{$_SESSION['titre']}', '{$_SESSION['contenu']}', '{$_SESSION['categorie']}', '{$_SESSION['pseudo']}', null)");
+        $nb_lignes = mysql_query("SELECT COUNT(*) as rowcount FROM article");
+        if($nb_lignes==0){
+            $sql = mysql_query("INSERT INTO article (idArticle, titreArticle, descriptionArticle, categorieArticle, pseudo, commentaires) VALUES (1, '{$_SESSION['titre']}', '{$_SESSION['contenu']}', '{$_SESSION['categorie']}', '{$_SESSION['pseudo']}', null)");
+        }
+        else{
+            $idmax = mysql_query("SELECT max(id_Article) FROM article");
+            $idmax = $idmax+1;
+            $sql = mysql_query("INSERT INTO article (idArticle, titreArticle, descriptionArticle, categorieArticle, pseudo, commentaires) VALUES (idmax, '{$_SESSION['titre']}', '{$_SESSION['contenu']}', '{$_SESSION['categorie']}', '{$_SESSION['pseudo']}', null)");
+        }
 
         header('Location: index.php');
         exit;
