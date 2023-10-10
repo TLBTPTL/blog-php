19496e60583cd95029b30a801503a718990ea40d
ANDRE <titouan-lysandre.andre@etu.univ-lyon1.fr>
2023-10-10 23:00:00 +0200

    avancée suppression article

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
+<?php
+include_once 'header.php';
+include_once 'functionDataBase.php';
+$connexion = connectToDatabase();
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,34 +11,21 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Page Article - Blog</title>
     <link rel="stylesheet" href="css/style.css">
-
 </head>
-<?php
-include_once 'header.php';
-include_once 'functionDataBase.php';
-$connexion = connectToDatabase();
-$_SESSION['erreurconnexion'] = 0;
-?>
 <body>
 <?php
-
 $sql = "SELECT idArticle, titreArticle, descriptionArticle, categorieArticle, pseudo FROM article";
 $stmt = $connexion->query($sql);
 $articles = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-
 foreach ($articles as $article) {
-    echo '<a href="article.php?id=' . $article['idArticle'] . '" class="article-link">';
+    echo "<a href='article.php?id={$article['idArticle']}' class='article'>";
     echo "<h2>{$article['titreArticle']}</h2>";
     echo "<p><strong>Catégorie:</strong> {$article['categorieArticle']}</p>";
     echo "<p><strong>Auteur:</strong> {$article['pseudo']}</p>";
     echo "<p>{$article['descriptionArticle']}</p>";
     echo '</a>';
 }
-
 ?>
-</body>
-</html>
-
 </body>
 </html>
